50cffbb3e2dbbb7b2c6aa2fbc28fd2f7c1d362d8
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-10-13 18:38:37 +0300

    Add files via upload

---
+++ work5.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//1
+/*
+	for (int i = 1; i <= 5; i = i + 1) {
+		cout << i << endl;
+	}
+*/
+//2
+/*
+	for (int i = 5; i >= 1; i = i - 1) {
+		cout << i << endl;
+	}
+*/
+//3
+/*
+	for (int i = 1; i <= 10; i = i + 1) {
+		cout << i * 3 << endl;
+	}
+*/
+//4
+/*
+	int sum=0;
+	int a;
+	cin >> a;
+	for (int i = 1; i <= a; i = i + 1) {
+		sum = sum + i;
+	}
+	cout << sum << endl;
+*/
+//for 1
+/*
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i = i + 1) {
+		cout << k << endl;
+	}
+*/
+//for 2
+/*
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <=b; i = i + 1) {
+		cout << i << endl;
+	}
+	cout << b - a + 1 << endl;
+*/
+//for 3
+/*
+	int a, b;
+	cin >> a >> b;
+	for (int i = b-1; i > a; i = i - 1) {
+		cout << i << endl;
+	}
+	cout << "n" << (b - 1) - a << endl;
+*/
+//for 4
+/*
+	int a, b;
+	cin >> a >> b;
+	for (int i = 1; i <= 10; i = i + 1) {
+		b = a * i;
+		cout << b << endl;
+	}
+*/
+//5
+/*
+	double a, b;
+	cin >> a;
+	for (double i = 0.1; i <= 1; i = i + 0.1) {
+		b = a * i;
+		cout << b << endl;
+	}
+*/
+//6
+/*
+	double a, b;
+	cin >> a;
+	for (double i = 1.2; i <= 2; i = i + 0.2) {
+		b = a * i;
+		cout << b << endl;
+	}
+*/
+//7
+/*	
+	int sum = 0;
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i = i + 1) {
+		sum = sum + i;
+	}
+	cout << sum << endl;
+*/
+//8
+/*
+	int sum = 1;
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i = i + 1) {
+		sum = sum * i;
+	}
+	cout << sum << endl;
+*/
+//9
+/*
+	int sum = 0;
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i = i + 1) {
+		sum = sum + (i * i);
+	}
+	cout << sum << endl;
+*/
+//10
+
+		
+	
+
+
+
+
+	return 0;
+}
